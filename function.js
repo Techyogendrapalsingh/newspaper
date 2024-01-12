e1fcd4a925d3b86addccb90c088ec1ad12eb89b3
Yogendra Pal <153979248+Techyogendrapalsingh@users.noreply.github.com>
2024-01-12 18:40:41 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,103 @@
+function head(Bsr){
+    document.write(`
+    <!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${Bsr}</title>
+      <link rel="stylesheet" href="style.css">     
+   </head>
+   <body>
+      <div class="head">
+         <a href="#" id="title">${Bsr}</a>
+      </div>
+
+
+    `);
+}
+
+function nav(){
+    document.write(`
+    <div class="nav">
+    <ul>
+       <li><a href="index.html">Home</a></li>
+       <li><a href="page.html">Bulandshahr</a></li>
+       <li><a href="page02.html">Ghaziabad</a></li>
+       <li><a href="About.html">About us</a></li>
+       <li><a href="Contact.html">Contact us</a></li>
+    </ul>
+ </div>
+ <div class="mid">
+    `);
+}
+
+function footer(){
+    document.write(`
+    </div>
+      <div class="footer">
+         <h4>2024@Copyright & Copy</h4>
+      </div>
+   </body>
+</html>
+    `);
+}
+
+function post(){
+    document.write(`
+    <div class="post">
+    <h2>When darkness overspreads my eyes</h2>
+    <p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream;
+               and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God!</p>
+
+<h1>MAY THE FORCE BE WITH YOU</h1>
+<p>Partially, but it also obeys your commands. Hey, Luke! May the Force be with you. I have traced the Rebel spies to her. Now she is my only link to finding their secret base.</p>
+
+<img src="images/baby02.avif" alt="banner" width="100%">  
+    </div>
+    
+    `)
+}
+
+function post01(){
+    document.write(`
+    <div class="post01">
+    <h1>GIVE PEOPLE COURAGE</h1>
+    <p>The sunset faded to twilight before anything further happened. The crowd far away on the left, towards Woking, seemed to grow, and I heard now a faint murmur from it. The little knot of people towards Chobham dispersed. There was scarcely an intimation of movement from the pit.</p>
+    <img src="images/baby01.avif" alt="banner" width="100%">
+    <p>It was this, as much as anything, that gave people courage, and I suppose the new arrivals from Woking also helped to restore confidence. At any rate, as the dusk came on a slow, intermittent movement upon the sand pits began, a movement that seemed to gather force as the stillness of the evening about the cylinder remained unbroken. Vertical black figures in twos and threes would advance, stop, watch, and advance again, spreading out as they did so in a thin irregular crescent that promised to enclose the pit in its attenuated horns. I, too, on my side began to move towards the pit.
+
+    Then I saw some cabmen and others had walked boldly into the sand pits, and heard the clatter of hoofs and the gride of wheels. I saw a lad trundling off the barrow of apples. And then, within thirty yards of the pit, advancing from the direction of Horsell, I noted a little black knot of men, the foremost of whom was waving a white flag.</p>
+
+ </div>
+    
+    `)
+}
+
+function news(){
+    document.write(`
+    <div class="nwes">
+    <div class="row">
+        <div class="col01">
+        <img src="images/trainhit.avif" alt=""width="30%">
+        
+        <img src="images/gun.jpg" alt=""width="30%">
+        
+        
+        </div>
+        <div class="col">
+        
+        <h3>Delhi-bound train hits iron girder pieces lying on railway track in UP's bulandshahr</h3>
+        <p>A Delhi-bound train hit some iron girder pieces lying on a railway track at the Khurja junction in bulandshahr, affecting the rail traffic for nearly an hour. The Jharkhand Swarna Jayanti Express, a train moving from Jharkhand, collided with the iron girder pieces of almost two meters</p>
+        <div class="hed">
+        <h3>Unable to execute murder, contract killers shoot hirer</h3>
+        </div>
+        <div class="hed01">
+        <h3>Pet dog attacks 4-year-old girl on b'day eve in bulandshahr, owner held</h3>
+        </div>
+        </div>
+    </div>
+   </div>
+
+    `)
+}
